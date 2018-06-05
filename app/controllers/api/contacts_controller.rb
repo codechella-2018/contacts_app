ad951401884fb36d425d4f0592e3826380c51990
dani <dani.zaghian@gmail.com>
2018-06-04 17:37:28 -0700

    contacts_app_rails52

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,13 +1,44 @@
 class Api::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render 'index.json.jbuilder'
+  end
 
-	def contact_action
-		@contact = Contact.first #hash
-		render "contact_view.json.jbuilder"
-	end
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find(contact_id)
+    render 'show.json.jbuilder'
+  end
 
-	def contacts_action
-		@contacts = Contact.all #array
-		render "contacts_view.json.jbuilder"
-	end
+  def create
+    @contact = Contact.new(
+     first_name: params[:first_name],
+     last_name: params[:last_name],
+     email: params[:email],
+     phone_number: params[:phone_number]
+    )
 
+    @contact.saveå
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    contact_id = params[:id]
+    @contact = Contact.find(contact_id)
+
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+    
+    @contact.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    contact_id = params[:id]
+    @contact = Contact.find(contact_id)
+    @contact.destroy
+    render json: {message: "contact successfully destroyed"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
   namespace :api do
-  	get "/contact_url" => "contacts#contact_action"
-  	get "/contacts_url" => "contacts#contacts_action"
+    get '/contacts' => 'contacts#index'
+    get '/contacts/:id' => 'contacts#show'
+    post '/contacts' => 'contacts#create'
+    patch '/contacts/:id' => 'contacts#update'
+    delete '/contacts/:id' => 'contacts#destroy'
   end
-end
+end 
